0c33e5bf37f56f3050482b5ac8b5dabf39d75d40
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2022-10-26 18:46:21 +0000

    made modifications to data insertion

--- index.js
+++ index.js
@@ -57,6 +57,14 @@ const insertData = () => {
 
     let dataArr = data.split("\n");
     for (let i = 1; i < dataArr.length; i++) {
+      const first_name = dataArr[i].split(",")[0];
+      const middle_name = dataArr[i].split(",")[1];
+      const date_of_graduation = dataArr[i].split(",")[2];
+      const date_of_employment = dataArr[i].split(",")[3];
+      const position = dataArr[i].split(",")[4];
+      const salary = dataArr[i].split(",")[5];
+      const supervisor = dataArr[i].split(",")[6];
+      const employee_code = dataArr[i].split(",")[7];
       // console.log(
       //   dataArr[i].split(",")[0],
       //   dataArr[i].split(",")[1],
@@ -70,14 +78,14 @@ const insertData = () => {
       db.run(
         sql,
         [
-          dataArr[i].split(",")[0],
-          dataArr[i].split(",")[1],
-          dataArr[i].split(",")[2],
-          dataArr[i].split(",")[3],
-          dataArr[i].split(",")[4],
-          dataArr[i].split(",")[5],
-          dataArr[i].split(",")[6],
-          dataArr[i].split(",")[7],
+          first_name,
+          middle_name,
+          date_of_graduation,
+          date_of_employment,
+          position,
+          salary,
+          supervisor,
+          employee_code,
         ],
         (err) => {
           if (err) return console.error(err.message);
